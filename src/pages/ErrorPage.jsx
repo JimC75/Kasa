c33429a22c3a0568acee2ee9ecd97ac8c7a7d533
jimc75 <jeanmariec.pro@gmail.com>
2023-10-24 16:36:10 +0200

    error page CSS

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -10,7 +10,7 @@ export default function ErrorPage() {
             <Header />
             <main className="container">
                 <div className='error'>
-                    <p className="quatre">Erreur de Chargement</p>
+                    <p className="error">Erreur de Chargement</p>
                     <p className="oops">Oups! La page que vous demandez n'existe pas.</p>
                     <Link to='/' className='retour'>Retourner à la page d'accueil</Link>
                 </div>
